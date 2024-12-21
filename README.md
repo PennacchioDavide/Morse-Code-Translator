6de14781ca7f2d355ba32b0c2863ea4d49451313
PennacchioDavide <dapennacchio@hotmail.com>
2024-12-21 13:16:01 +0100

    last version

---
+++ README.md
@@ -0,0 +1,5 @@
+This Program translate morse code into normal language
+
+Its based on the international morse code
+
+Between words you have to put 2 spaces and 1 between letters in morse code
--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 #include <stdlib.h>
 
 int main (int argc, char *argv[]) {
-    const char *morse = "-.. .- ...- .. -.. .  ..--- -----";
+    const char *morse = argv[1];
+    
     char **morse_words = morse_phrase_to_morse_word_list(morse);
     char **words = morse_word_list_to_word_list(morse_words);
     char *phrase = word_list_to_phrase(words);
 
     printf("%s\n", phrase);
 
-    // Libération de la mémoire
     free_word_list(morse_words);
     free_word_list(words);
     free(phrase);
